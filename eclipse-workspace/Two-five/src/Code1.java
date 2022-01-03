70ff07443f4528420ec696d066a88d9bdd23fa1d
minkyung kim <minki513@naver.com>
2022-01-03 14:56:36 +0900

    변수값출력

---
+++ eclipse-workspace/Two-five/src/Code1.java
@@ -0,0 +1,13 @@
+
+public class Code1 {
+
+	public static void main(String[] args) {
+		int a = 10, b = 27;
+		
+		System.out.println("a is " +a);
+		System.out.println("b is " +b);
+		System.out.println("a+b is " +(a + b));
+
+	}
+
+}
