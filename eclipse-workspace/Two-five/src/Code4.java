9728241209ee0b72ccde3c41cba7c184f8c79aec
minkyung kim <minki513@naver.com>
2022-01-03 15:10:50 +0900

    연산자

---
+++ eclipse-workspace/Two-five/src/Code4.java
@@ -0,0 +1,22 @@
+
+public class Code4 {
+
+	public static void main(String[] args) {
+		// 수지 연산자
+		int a = 30, b=7;
+		int c, d, e, f, g;
+		
+		c = a + b;
+		d = a-b;
+		e = a*b;
+		f = a%b;
+		g = a/b;
+		
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+	}
+
+}
