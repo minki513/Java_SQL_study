8b4c11eefea461f053c59aa8cd6331bbfe1a6ef4
minkyung kim <minki513@naver.com>
2022-01-03 15:08:31 +0900

    10,2,8,16진수

---
+++ eclipse-workspace/Two-five/src/Code3.java
@@ -0,0 +1,23 @@
+
+public class Code3 {
+
+	public static void main(String[] args) {
+		float f1 = 5.3F;
+		float f2 = 3.775f;
+		float f3;
+		f3 = f1 +f2;
+		System.out.println(f3);
+		
+		// 10진수, 2진수, 8진수, 16진수
+		int a = 10;
+		int b = 0b1010;
+		int c = 012;
+		int d = 0xA;
+		System.out.println("a: " + a);
+		System.out.println("b: " + b);
+		System.out.println("c: " + c);
+		System.out.println("d: " + d);
+
+	}
+
+}
