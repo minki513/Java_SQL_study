30ebbfba48ec1278284a883183eb1be64356fea5
minki513 <89339498+minki513@users.noreply.github.com>
2022-01-14 19:50:36 +0900

    ListIterator

---
+++ eclipse-workspace/Two-five/src/generics/Generics9.java
@@ -0,0 +1,26 @@
+package generics;
+
+import java.util.*;
+
+public class Generics9 {
+
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("1");
+		list.add("2");
+		list.add("3");
+		list.add("4");
+		list.add("5");
+		
+		ListIterator<String> it = list.listIterator();
+		
+		while(it.hasNext())
+			System.out.print(it.next());
+		
+		System.out.println();
+		
+		while(it.hasPrevious())
+			System.out.print(it.previous());
+	}
+
+}
